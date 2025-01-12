527d542ba9870249be52f77cc31ad54b763e06ec
unknown <mehedytouhidurzaman@gmail.com>
2025-01-12 16:10:02 +0600

    The axios intercepter is implemented in the axiosSecure hook

--- client/src/Hooks/useAxiosSecure.jsx
+++ client/src/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_url,
@@ -6,6 +8,20 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const {logOut} = useAuth()
+  const navigate = useNavigate()
+  axiosSecure.interceptors.response.use(
+    res=>{
+      return res
+    },
+   async err=>{
+      if(err.response.status === 401 || err.response.status === 403){
+        await logOut
+        navigate("/login")
+      }
+      return Promise.reject(err);
+    }
+  )
     return axiosSecure
 };
 
